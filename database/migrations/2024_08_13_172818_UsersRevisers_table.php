c8441006ae3b2776dc1a898a09a7932196b9806e
Farah Khaled <farahkhaled03@gmail.com~>
2024-08-14 13:21:23 +0300

    The migrations file-update 3

--- database/migrations/2024_08_13_172818_UsersRevisers_table.php
+++ database/migrations/2024_08_13_172818_UsersRevisers_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users_revisers', function (Blueprint $table) {
+        Schema::create('UsersRevisers', function (Blueprint $table) {
             $table->id();
             $table->integer('balance')->default(0);
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users_revisers');
+        Schema::dropIfExists('UsersRevisers');
     }
 };
