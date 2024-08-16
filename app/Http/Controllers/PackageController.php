9cc831e3e84f9e15a676a00b3866e330d0cff135
MohamedMaher03 <m7mdrefaat550@gmail.com>
2024-08-16 04:11:49 +0300

    finish buy package

---
+++ app/Http/Controllers/PackageController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Organization;
+use App\Models\Package;
+use Illuminate\Http\Request;
+
+class PackageController extends Controller
+{
+    public function buyPackage($id)
+    {
+        $user = auth()->user();
+
+        if (!$user || $user->role_id !== 1) {
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'Unauthorized access'
+            ], 403);
+        }
+
+        $package = Package::find($id);
+
+        if(!$package){
+            return response()->json([
+               'status' => 'failed',
+               'message' => 'Package not found'
+            ],404);
+        }
+        $operationCount=$package->operations_count;
+
+        $organization = Organization::where('user_id', $user->id)->first();
+        if (!$organization) {
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'Organization not found'
+            ], 404);
+        }
+        //$organization=Organization::find(11);;
+        $organizationCount=$organization->operations_count;
+        $organizationCount+=$operationCount;
+        $organization->update(['operations_count'=>$organizationCount]);
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Package buy successfully'
+        ]);
+    }
+}
